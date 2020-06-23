052cb09ce3c24b7e19a6a8ce0bd44eb05d97069d
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-23 18:55:42 +0100

    get_fish_form_river bear test passes

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -10,6 +10,16 @@ class BearTest < MiniTest::Test
 
     def setup()
         @bear = Bear.new("Yogi", "Grizzly")
+
+        @fish1 = Fish.new("Salmon")
+        @fish2 = Fish.new("Bream")
+        @fish3 = Fish.new("Tuna")
+        @fish4 = Fish.new("Trout")
+        @fish5 = Fish.new("Eel")
+
+        @fishes = [@fish1, @fish2, @fish3, @fish4, @fish5]
+
+        @river = River.new("Amazon", @fishes)
     end
 
     def test_bear_name()
@@ -24,8 +34,13 @@ class BearTest < MiniTest::Test
         assert_equal(0, @bear.food_count())
     end
 
-    def test_get_fish_from_river()
-
+    def test_gets_fish_from_river()
+        @river.remove_fish(@fish1)
+        @river.remove_fish(@fish2)
+        @bear.add_to_stomach(@fish1)
+        @bear.add_to_stomach(@fish2)
+        assert_equal(3, @river.count_fish())
+        assert_equal(2, @bear.food_count())
     end
 
     def test_roar()
